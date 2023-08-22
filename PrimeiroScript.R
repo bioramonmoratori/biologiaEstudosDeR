c8c13b15c2637e55383fe23d454ba3e6e9f2294d
root <root@ramon>
2023-08-22 17:02:12 -0300

    primeiro projeto R

--- PrimeiroScript.R
+++ PrimeiroScript.R
@@ -12,3 +12,18 @@ citation()
 
 # Demonstracoes de graficos e diversas outras funcionalidades do R
 demo()
+
+file
+
+# Documentacao da funcao Importar Ler Tabela
+?read.table(file)
+
+## Muito importante as funcionalidades de configuracao da separacao dos arquivos
+## Exemplo: csv tem uma delimitacao ;
+# Importamos um pacote de Excel para o RStudio nao desconfigurar acentos e espacos
+# h = cabecalho true  ou false da planilha
+minhaPlanilha <- read.table("bancodedados.csv", h = T)
+
+write.csv(minhaPlanilha, "dados.txt")
+
+
