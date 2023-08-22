aa70dc0edfb546859548efaf068e8bbbb089fe20
root <root@ramon>
2023-08-22 16:17:13 -0300

    primeiro projeto R

---
+++ PrimeiroScript.R
@@ -0,0 +1,14 @@
+
+# Primeiras funcoes no R
+ 
+# Seleciona o diretorio de trabalho
+setwd()
+
+# Pega o diretorio de trabalho
+getwd()
+
+# Mostra como podemos citar o programa em artigos
+citation()
+
+# Demonstracoes de graficos e diversas outras funcionalidades do R
+demo()
